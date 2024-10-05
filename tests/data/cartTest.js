e43bb892c0dd02d5b199df717b8302471ccc52a9
oofstruggle <Chusova2003@mail.ru>
2024-10-05 11:57:51 +0500

    Add test suite removeFromCart function

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
   beforeEach(() => {
@@ -45,5 +45,49 @@ describe('test suite: addToCart', () => {
     }]));
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(1);
+  });
+});
+
+describe('test suite: removeFromCart', () => {
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+  });
+
+  it('removes a productId that is in the cart', () => {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+
+    removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(0);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([]));
+  });
+
+  it('does nothing if product is not in the cart', () => {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+
+    removeFromCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(2);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }]));
   });
 });
