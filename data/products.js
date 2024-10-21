09282eade7221a07a278425b8f1d53be7c4b6ede
oofstruggle <Chusova2003@mail.ru>
2024-10-21 22:59:32 +0500

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -128,6 +128,8 @@ export function loadProductsFetch() {
       return new Product(productDetails);
     });
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -155,6 +157,10 @@ export function loadProducts(fun) {
     console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
